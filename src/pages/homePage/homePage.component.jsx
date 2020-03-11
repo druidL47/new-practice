57c27f413f1ad14b753474d9421b60402de03d73
derek <derek4477@hotmail.com>
2020-03-11 09:14:05 -0700

    broken our homepage into smaller component.

---
+++ src/pages/homePage/homePage.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./homePage.style.scss";
+import Directory from "../../components/home-directory/directory.component";
+
+const HomePage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
+export default HomePage;
